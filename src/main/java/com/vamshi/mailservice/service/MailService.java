d4328adfd9fd78457576908dc4f0cf13760ab626
vamshi-muktha <mukthavamshi123@gmail.com>
2026-01-13 01:23:38 +0530

    Use Brevo Email API

--- src/main/java/com/vamshi/mailservice/service/MailService.java
+++ src/main/java/com/vamshi/mailservice/service/MailService.java
@@ -1,23 +1,71 @@
+//package com.vamshi.mailservice.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MailService {
+//	
+//	@Autowired
+//    private JavaMailSender mailSender;
+//
+//	public void sendMail(String email, String subject, String text) {
+//		SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(email);
+//        message.setSubject(subject);
+//        message.setText(text);
+//
+//        mailSender.send(message);
+//	}
+//
+//}
+
+
 package com.vamshi.mailservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MailService {
-	
-	@Autowired
-    private JavaMailSender mailSender;
 
-	public void sendMail(String email, String subject, String text) {
-		SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(subject);
-        message.setText(text);
+    @Value("${brevo.api.url}")
+    private String url;
 
-        mailSender.send(message);
-	}
+    @Value("${brevo.api.key}")
+    private String apiKey;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendMail(String to, String subject, String body) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("api-key", apiKey);
+
+        String json = """
+        {
+          "sender": {
+            "email": "mukthavamshi123@gmail.com",
+            "name": "SecureCard"
+          },
+          "to": [
+            { "email": "%s" }
+          ],
+          "subject": "%s",
+          "htmlContent": "<p>%s</p>"
+        }
+        """.formatted(to, subject, body);
+
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+
+        restTemplate.postForObject(url, request, String.class);
+    }
 }
+
