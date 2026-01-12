a82465325a2a91b2495166a959af0674018f22fd
vamshi-muktha <mukthavamshi123@gmail.com>
2026-01-13 00:19:46 +0530

    mail

--- src/main/java/com/vamshi/mailservice/MailServiceApplication.java
+++ src/main/java/com/vamshi/mailservice/MailServiceApplication.java
@@ -2,8 +2,6 @@ package com.vamshi.mailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
